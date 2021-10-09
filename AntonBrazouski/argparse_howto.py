8af34d616240b30f9c5a824389f9f08c0adad031
AntonBrazouski <antonbrazouski@gmail.com>
2021-10-09 12:05:17 +0300

    argparse how to tutorial

---
+++ AntonBrazouski/argparse_howto.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+# parser. add_argument("checho", help="checho is the string you see here!")
+# parser.add_argument("square", help='display a square of a given number',
+#    type=int)
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
+
+# print(args.square**2)
