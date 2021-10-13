15210d2786ac14fded405a351b800f3fdd931982
AntonBrazouski <antonbrazouski@gmail.com>
2021-10-13 21:06:28 +0300

    Task_7-6.py

---
+++ AntonBrazouski/Task_7-6.py
@@ -0,0 +1,45 @@
+def gc():
+    number = input("please enter a number: ")
+    if number == 'q':
+        return
+    else:
+        number = int(number)
+
+    print('result: ')
+
+
+gc()
+
+
+# make use of 5.5 func
+
+last_result = None
+
+
+def remember_result(func):
+
+    def inner(*args):
+        global last_result 
+        print(f"Last result = {last_result}")
+        last_result = func(*args)
+
+    return inner
+
+
+@remember_result
+def sum_list(*args):  
+    result = ""
+    if isinstance(args[0], int):
+        result = 0
+
+    for item in args:
+        result += item
+    
+    print(f"Current result = '{result}'")
+
+    return result
+
+
+sum_list("a", "b")
+sum_list("abc", "cde")
+sum_list(3, 4, 5)
